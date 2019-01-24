722e6f710eac2c56fecbd80d62c75d6d1636bff2
MarcinBaranczak <zp34q@wp.pl>
2019-01-24 09:52:07 +0100

    budowanie statku

--- Table.py
+++ Table.py
@@ -1,3 +1,5 @@
+import random
+
 class Table:
     def __init__(self, maxTabSize):
         self._tabA = [[0 for row in range(maxTabSize)] for col in range(maxTabSize)]
@@ -12,20 +14,34 @@ class Table:
     def _getFieldValue(self, col, row):                 # zwraca wartosc podanego pola
         return self._tabA[col][row]
 
-    def _checkField(self, col, row, value):             # sprawdza dane pole i pola obok(te mogą miećzadaną wartość)
+    def _checkField(self, col, row, val):               # sprawdza dane pole i pola obok(te mogą mieć zadaną wartość)
         if self._getFieldValue(col, row) != 0:          # czy wybrane pole jest puste(wartość 0), jeżeli jest wartość - wychodzi
             return False
 
         for i in range(3):                              # czy przyległe pola są puste, lub mają zadaną wartość. Jeżeli nie - wychodzi
             for j in range(3):
-                if self._getFieldValue(col-1+i, row-1+i) != 0  and  self._getFieldValue(col-1+i, row-1+i) != value:
+                if self._getFieldValue(col-1+i, row-1+i) != 0  and  self._getFieldValue(col-1+i, row-1+i) != val:
                     return False
 
         return True
 
+    def _setField(self, col, row, val):                 # ustawia daną wartość w danym polu, zwraca true, jeżeli się uda
+        if self._checkField(col, row, val) == True:
+            self._tabA[col][row] = val
+            return True
+
+        return False
+
+    def _placeShip(self, val):                          #buduje statek, ustawia odpowiednie wartości w pola
+        for i in range (5):
+            col = random.randint(1, 10)
+            row = random.randint(1, 10)
+            self._tabA[col][row] = random.randint(10, 90)
+
+
     def doStuff(self):                                  # funkcja testowa
         self._borderFill()
-        print(self._checkField(2, 3, 10))
+        self._placeShip(10)
         for i in range(len(self._tabA)):                # kolumny
             for j in range(len(self._tabA)):            # wiersze
                 if self._tabA[i][j] < 0:
